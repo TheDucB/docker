3c1b4b18b1514838bbc872b4c2ffe1f467d3e36c
The Duc Bui <thbui@stud.hs-heilbronn.de>
2020-11-12 21:28:55 +0100

    added checkstyle

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("This is a helloWOrld object!");
+
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
